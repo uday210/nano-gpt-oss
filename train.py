3f41aa11899aee2c1230ad56262c42ff51819406
uday210 <udaykiran.uday@gmail.com>
2025-10-22 12:35:58 -0700

    Update model setup and text generation
    Refactor model initialization and output generation.

--- train.py
+++ train.py
@@ -1,20 +1,22 @@
-from training.data_loader import train_loader,val_loader
-from architecture.gptoss import Transformer,ModelConfig
+from training.data_loader import train_loader, val_loader
+from architecture.gptoss import Transformer, ModelConfig
 import torch
 from inference import generate_text
-device= "cuda:0"
-context="Once upon a day"
-model= Transformer(ModelConfig(
-    num_attention_heads=8
-    ,num_key_value_heads=4,
+
+device = "cuda:0"  # or "cpu" if no GPU
+context = "Once upon a day"
+model = Transformer(ModelConfig(
+    num_attention_heads=8,
+    num_key_value_heads=4,
     num_experts=4,
     experts_per_token=1,
     num_hidden_layers=12,
-    hidden_size =1024,
-    intermediate_size = 1024
-    
-    ),device)
-print(sum([p.numel() for p in model.parameters()])/1000000,"M parameaters")
-torch.save(model.state_dict(),"model/gptoss.pt")
+    hidden_size=1024,
+    intermediate_size=1024
+), device)
 
-generate_text(model,context)
+print(sum(p.numel() for p in model.parameters()) / 1e6, "M parameters")
+torch.save(model.state_dict(), "model/gptoss.pt")
+
+generated = generate_text(model, context, max_tokens=100)
+print(generated)  # this will display the generated text
